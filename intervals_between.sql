b8eafd4ca8accef7d5b3429d0cad6eddae0be88a
Dan Barry <dan@chaptercommunications.com>
2008-04-24 15:02:46 -0500

    optimize intervals_between() using multiplier

--- intervals_between.sql
+++ intervals_between.sql
@@ -8,12 +8,17 @@ CREATE OR REPLACE FUNCTION  intervals_between(
   AS $BODY$
 DECLARE
   count FLOAT := 0;
+  multiplier INT := 512;
 BEGIN
   IF start_date > end_date THEN
     RETURN 0;
   END IF;
-  WHILE start_date + count * duration < end_date LOOP
-    count := count + 1;
+  LOOP
+    WHILE start_date + (count + multiplier) * duration < end_date LOOP
+      count := count + multiplier;
+    END LOOP;
+    EXIT WHEN multiplier = 1;
+    multiplier := multiplier / 2;
   END LOOP;
   count := count + (extract(epoch from end_date) - extract(epoch from (start_date + count * duration))) / (extract(epoch from end_date + duration) - extract(epoch from end_date))::int;
   RETURN count;
