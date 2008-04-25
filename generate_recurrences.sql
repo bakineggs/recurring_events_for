afc00d0597f82038014ac0c45b8daf2a01a13af3
Dan Barry <dan@chaptercommunications.com>
2008-04-25 16:34:57 -0500

    pass tests

--- generate_recurrences.sql
+++ generate_recurrences.sql
@@ -11,25 +11,23 @@ CREATE OR REPLACE FUNCTION  generate_recurrences(
 DECLARE
   next_date DATE;
 BEGIN
-  next_date := original_date + duration * (CEIL(intervals_between(original_date, range_start, duration))-1);
+  next_date := original_date + duration * CEIL(intervals_between(original_date, range_start, duration));
   IF pattern_type = 'positive_week_dow' OR pattern_type = 'negative_week_dow' THEN
+    -- in case the first recurrence is before next_date but after range_start
+    next_date := next_date - 2 * duration;
     LOOP
-      -- increase by 2 durations if 1 will put us in the same month
-      IF extract(year from next_date + duration) = extract(year from next_date) AND extract(month from next_date + duration) = extract(month from duration) THEN
+      -- prevent an infinite loop
+      IF duration = '28 days'::interval AND extract(month from next_date + duration) = extract(month from next_date) THEN
         next_date := next_date + duration;
       END IF;
       next_date := next_date + duration;
 
-      -- Yearly events could be put in the wrong month since 364 days is less than a year.
-      -- This has to be a while loop since 28 days is sometimes less than a month,
-      -- so next_date might not be pushed all the way into the right month.
-      WHILE duration = '364 days'::interval AND extract(month from next_date) != extract(month from original_date) LOOP
-        IF extract(month from next_date) > extract(month from original_date) THEN
-          next_date := next_date + (12 - (extract(month from next_date) - extract(month from original_date))) * '28 days'::interval;
-        ELSE
-          next_date := next_date + (extract(month from original_date) - extract(month from next_date)) * '28 days'::interval;
-        END IF;
-      END LOOP;
+      -- ensure month is correct for yearly events
+      IF duration = '364 days'::interval THEN
+        WHILE extract(month from next_date) != extract(month from original_date) LOOP
+          next_date := next_date + cast(extract(month from original_date) - extract(month from next_date) as int) % 12 * '28 days'::interval;
+        END LOOP;
+      END IF;
 
       IF pattern_type = 'positive_week_dow' THEN
         next_date := next_date
@@ -38,18 +36,18 @@ BEGIN
           * '7 days'::interval;
       ELSE
         next_date := next_date
-          + (FLOOR((extract(day from original_date + '1 month'::interval - original_date) - extract(day from original_date)) / 7)
-            - FLOOR((extract(day from next_date + '1 month'::interval - next_date) - extract(day from next_date)) / 7))
+          + (CEIL((extract(day from next_date + '1 month'::interval - next_date) - extract(day from next_date)) / 7)
+              - CEIL((extract(day from original_date + '1 month'::interval - original_date) - extract(day from original_date)) / 7))
           * '7 days'::interval;
       END IF;
       EXIT WHEN next_date > range_end;
+      CONTINUE WHEN next_date < range_start; -- subtracting an extra duration could have put us before the range_start
       RETURN NEXT next_date;
     END LOOP;
   ELSE
-    LOOP
-      next_date := next_date + duration;
-      EXIT WHEN next_date > range_end;
+    WHILE next_date <= range_end LOOP
       RETURN NEXT next_date;
+      next_date := next_date + duration;
     END LOOP;
   END IF;
 END;
