192865ca9c142ee15da7bbaec2e80782eba99fc7
Dan Barry <dan@chaptercommunications.com>
2008-07-09 11:53:54 -0500

    condense redundant specs

--- spec/recurring_events_for_spec.rb
+++ spec/recurring_events_for_spec.rb
@@ -276,6 +276,21 @@ describe 'recurring_events_for' do
       ]
     end
 
+    describe 'mutliple recurrence rules' do
+      it "should only include events for count recurrences" do
+        executing([
+          "insert into events (id, date, frequency, count) values (1, '2008-04-25', 'monthly', 3);",
+          "insert into event_recurrences (event_id, day) values (1, 28);",
+          "insert into event_recurrences (event_id, day) values (1, 4);",
+          "select distinct date from recurring_events_for('2008-04-01 12:00pm', '2008-06-25 12:00pm', 'UTC', NULL);"
+        ]).should == [
+          ['2008-04-25'],
+          ['2008-04-28'],
+          ['2008-05-04']
+        ]
+      end
+    end
+
     describe 'cancellations' do
       it "should not include cancelled recurrences" do
         executing([
@@ -403,19 +418,6 @@ describe 'recurring_events_for' do
             ['2008-05-04']
           ]
         end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2008-04-25', 'monthly', 3);",
-            "insert into event_recurrences (event_id, day) values (1, 28);",
-            "insert into event_recurrences (event_id, day) values (1, 4);",
-            "select distinct date from recurring_events_for('2008-04-01 12:00pm', '2008-06-25 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2008-04-25'],
-            ['2008-04-28'],
-            ['2008-05-04']
-          ]
-        end
       end
 
       describe 'using a custom day of week in month' do
@@ -429,21 +431,6 @@ describe 'recurring_events_for' do
             ['2008-04-25'],
             ['2008-05-09'],
             ['2008-05-22']
-          ]
-        end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2008-04-25', 'monthly', 5);",
-            "insert into event_recurrences (event_id, week, day) values (1, 2, 5);",
-            "insert into event_recurrences (event_id, week, day) values (1, -2, 4);",
-            "select distinct date from recurring_events_for('2008-04-01 12:00pm', '2008-07-25 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2008-04-25'],
-            ['2008-05-09'],
-            ['2008-05-22'],
-            ['2008-06-13'],
-            ['2008-06-19']
           ]
         end
 
@@ -486,20 +473,6 @@ describe 'recurring_events_for' do
             ['2009-07-25']
           ]
         end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2008-04-25', 'yearly', 4);",
-            "insert into event_recurrences (event_id, month) values (1, 2);",
-            "insert into event_recurrences (event_id, month) values (1, 7);",
-            "select distinct date from recurring_events_for('2007-04-01 12:00pm', '2010-07-25 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2008-04-25'],
-            ['2008-07-25'],
-            ['2009-02-25'],
-            ['2009-07-25']
-          ]
-        end
       end
 
       describe 'using a custom day of month' do
@@ -509,20 +482,6 @@ describe 'recurring_events_for' do
             "insert into event_recurrences (event_id, day) values (1, 28);",
             "insert into event_recurrences (event_id, day) values (1, 7);",
             "select distinct date from recurring_events_for('2007-04-01 12:00pm', '2009-04-28 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2008-04-25'],
-            ['2008-04-28'],
-            ['2009-04-07'],
-            ['2009-04-28']
-          ]
-        end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2008-04-25', 'yearly', 4);",
-            "insert into event_recurrences (event_id, day) values (1, 28);",
-            "insert into event_recurrences (event_id, day) values (1, 7);",
-            "select distinct date from recurring_events_for('2007-04-01 12:00pm', '2010-04-28 12:00pm', 'UTC', NULL);"
           ]).should == [
             ['2008-04-25'],
             ['2008-04-28'],
@@ -550,24 +509,6 @@ describe 'recurring_events_for' do
             ['2009-07-07']
           ]
         end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2008-04-25', 'yearly', 6);",
-            "insert into event_recurrences (event_id, month, day) values (1, 2, 28);",
-            "insert into event_recurrences (event_id, month, day) values (1, 2, 7);",
-            "insert into event_recurrences (event_id, month, day) values (1, 7, 28);",
-            "insert into event_recurrences (event_id, month, day) values (1, 7, 7);",
-            "select distinct date from recurring_events_for('2007-04-01 12:00pm', '2010-07-07 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2008-04-25'],
-            ['2008-07-07'],
-            ['2008-07-28'],
-            ['2009-02-07'],
-            ['2009-02-28'],
-            ['2009-07-07']
-          ]
-        end
       end
 
       describe 'using a custom day of week in month' do
@@ -581,21 +522,6 @@ describe 'recurring_events_for' do
             ['2008-04-25'],
             ['2009-04-10'],
             ['2009-04-23']
-          ]
-        end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2008-05-25', 'yearly', 5);",
-            "insert into event_recurrences (event_id, week, day) values (1, 2, 5);",
-            "insert into event_recurrences (event_id, week, day) values (1, -2, 4);",
-            "select distinct date from recurring_events_for('2007-04-01 12:00pm', '2011-06-25 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2008-05-25'],
-            ['2009-05-08'],
-            ['2009-05-21'],
-            ['2010-05-14'],
-            ['2010-05-20']
           ]
         end
       end
@@ -616,24 +542,6 @@ describe 'recurring_events_for' do
             ['2009-02-13'],
             ['2009-02-19'],
             ['2009-07-10']
-          ]
-        end
-
-        it "should include count recurrences" do
-          executing([
-            "insert into events (id, date, frequency, count) values (1, '2009-04-25', 'yearly', 6);",
-            "insert into event_recurrences (event_id, month, week, day) values (1, 2, 2, 5);",
-            "insert into event_recurrences (event_id, month, week, day) values (1, 2, -2, 4);",
-            "insert into event_recurrences (event_id, month, week, day) values (1, 5, 2, 5);",
-            "insert into event_recurrences (event_id, month, week, day) values (1, 5, -2, 4);",
-            "select distinct date from recurring_events_for('2007-04-01 12:00pm', '2010-07-10 12:00pm', 'UTC', NULL);"
-          ]).should == [
-            ['2009-04-25'],
-            ['2009-05-08'],
-            ['2009-05-21'],
-            ['2010-02-12'],
-            ['2010-02-18'],
-            ['2010-05-14']
           ]
         end
       end
