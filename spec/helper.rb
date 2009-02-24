e91cf88d8cc305a9e386aed5f7c5959ec06bdb20
Kyle Hargraves <philodespotos@gmail.com>
2009-02-24 09:20:59 -0600

    let ruby do the work

--- spec/helper.rb
+++ spec/helper.rb
@@ -10,15 +10,15 @@ begin
     '',
     ''
   )
-  
+
   $db_conn.exec 'SET TIMEZONE TO UTC;'
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../events.sql').read
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../days_in_month.sql').read
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../interval_for.sql').read
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../intervals_between.sql').read
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../generate_recurrences.sql').read
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../recurrences_for.sql').read
-  $db_conn.exec File.open(File.dirname(__FILE__) + '/../recurring_events_for.sql').read
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../events.sql')
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../days_in_month.sql')
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../interval_for.sql')
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../intervals_between.sql')
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../generate_recurrences.sql')
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../recurrences_for.sql')
+  $db_conn.exec File.read(File.dirname(__FILE__) + '/../recurring_events_for.sql')
 rescue PGError
   puts "Failed to connect to and initialize database"
   raise
